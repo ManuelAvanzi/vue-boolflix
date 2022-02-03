604c37788268a7b432f2ceef0bfc64292a41496f
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-03 12:31:18 +0100

    header-component-logo

---
+++ src/components/headerContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header-box">
+
+      <div class="title-container">
+        <h1>Boolflix</h1>
+      </div>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+ 
+ .header-box{
+     background-color: rgba(0, 0, 0, 0.932);
+     .title-container{
+      
+       padding:5px 10px;
+             
+     h1{
+         color:rgb(216, 0, 0);
+       }
+     }
+ }
+
+</style>
