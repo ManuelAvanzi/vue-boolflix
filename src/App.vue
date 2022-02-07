3c2403e720e38d96d7d134061eb6a9813c5996a7
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-07 12:22:53 +0100

    film e serie components

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,67 @@
   <div id="app">
     <!--@nome evento custom ="nome della funzione in App.vue che userà il valore passato"-->
     <header-container @search="cercaFilm"/>
-    <main-container :elenco="films"/>
+    
+    <main-container :films="films" :series="series" lingua="linguaggi"/>
   </div>
 </template>
 
 <script>
 
 //API Key: f92de852056df36bad71c98feade9c89
-//https://api.themoviedb.org/3/search/movie?api_key=f92de852056df36bad71c98feade9c89 
 import axios from 'axios'
 import HeaderContainer from './components/headerContainer.vue'
 import MainContainer from './components/mainContainer.vue'
 
 
 export default {
+  
   name: 'App',
   components: {
     HeaderContainer,
     MainContainer,
   },
+
   data(){
+
     return {
       api_key:'f92de852056df36bad71c98feade9c89',
-      films:[],
-      series:[],
-      
-      stringA:"https://api.themoviedb.org/3/search/movie?query=",
-      stringB:"&api_key=f92de852056df36bad71c98feade9c89",
+      films: [],
+      series: [],
+      linguaggi:['italiano','brazilian','france','german','india','norway','portugal','uk']
 
     }
   },
   methods:{
 
       
-      //cercaFilm ora parte ogni volta che viene premuto il pulsante 
+     
       //userQuery è la variabile che ho definito in headerContainer.vue
       cercaFilm(userQuery){
-        console.log(userQuery);
+        
+        
 
-        console.log(this.stringA + userQuery + this.stringB)
-        
-        axios.get(this.stringA + userQuery + this.stringB).then((response)=>{
-        this.films=response.data.results;
-        console.log(this.films);
-        
+        //ricerco il film partendo dalla userQuery
+        axios.get(`https://api.themoviedb.org/3/search/movie?query=${userQuery}&api_key=f92de852056df36bad71c98feade9c89`)
+        .then((response) => {
+          this.films = response.data.results;
         });
+
+        //ricerco la serie partendo dalla userQuery
+        axios.get(`https://api.themoviedb.org/3/search/tv?query=${userQuery}&api_key=f92de852056df36bad71c98feade9c89`)
+        .then((response) => {
+          this.series = response.data.results;
+          
+        });
+        
+       
 
       
       }       
    },
+ 
+     
+   
                                            
   }
 
